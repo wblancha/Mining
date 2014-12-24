e8b4fd329a23a7b5b663517101f67e35a94e9dd3
Chaos <wblancha@gmail.com>
2014-12-24 07:22:35 -0500

    Adding functionality

--- Mining.py
+++ Mining.py
@@ -5,12 +5,5 @@ url = "https://www.electricarchaeology.ca"
 response = urllib2.urlopen(url)
 webContent = response.read()
 
-page = open("ore.txt", "w")
-page.write(webContent)
-page.close()
-
-refined = strip.StripTags("ore.txt")
-page2 = open("refined.txt", "w")
-page2.write(refined)
-page.close()
-#print refined
+text = strip.StripTags(webContent)
+print text
