b79d28dfbde219f39aeb4b75af184147e144ba7b
Chaos <wblancha@gmail.com>
2014-12-24 07:22:13 -0500

    adding an end location

--- strip.py
+++ strip.py
@@ -1,5 +1,5 @@
 def StripTags (PageContents):
     startLoc = PageContents.find('<div class="entry-content">')
     endLoc = PageContents.find("</div><!-- .entry-content -->")
-    PageContents = PageContents[startLoc:]
+    PageContents = PageContents[startLoc:endLoc]
     return PageContents
