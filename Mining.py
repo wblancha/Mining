a361bb7d56c469b7bb0942512bdefe2713c30424
Chaos <wblancha@gmail.com>
2014-12-31 07:22:57 -0500

    added counting feature

--- Mining.py
+++ Mining.py
@@ -1,9 +1,22 @@
+import count
 import urllib2
 import strip
-url = "https://www.electricarchaeology.ca"
+url = "http://www.gutenberg.org/files/35/35.txt"
 
 response = urllib2.urlopen(url)
 webContent = response.read()
 
-text = strip.StripTags(webContent)
-print text
+text = strip.StripTags(webContent).lower()
+final = strip.stripNonAlphaNum(text)
+#print text
+#print final
+file = open("refined.txt","w")
+file.write(str(final))
+file.close()
+for word in final:
+    file = open("fine.txt","a")
+    file.write("\n" + word)
+    file.close()
+counted = count.counting(final)
+
+print counted
