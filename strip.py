3f05380e92987021e6da3508064c2f34230c5060
Chaos <wblancha@gmail.com>
2014-12-31 07:23:19 -0500

    re commit

--- strip.py
+++ strip.py
@@ -1,5 +1,23 @@
 def StripTags (PageContents):
     startLoc = PageContents.find('<div class="entry-content">')
-    endLoc = PageContents.find("</div><!-- .entry-content -->")
-    PageContents = PageContents[startLoc:endLoc]
-    return PageContents
+    endLoc = PageContents.find("End of Project")
+    PageContents = PageContents[:endLoc]
+   # return PageContents
+
+    inside = 0
+    text = ""
+
+    for char in PageContents:
+        if char == "<":
+            inside = 1
+        elif (inside ==1 and char == ">"):
+            inside = 0
+        elif inside == 1:
+            continue
+        else:
+            text += char
+    return text    
+
+def stripNonAlphaNum(text):
+    import re
+    return re.compile(r"\W+",re.UNICODE).split(text)
