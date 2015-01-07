9e18f3337dae581ac7ccea594630bf7d7303e489
ZeroHour <wblancha@gmail.com>
2015-01-07 07:13:59 -0500

    |Removed unnecessary code, organized, comments

--- Mining.py
+++ Mining.py
@@ -1,22 +1,15 @@
 import count
 import urllib2
 import strip
-#url = "http://www.gutenberg.org/files/35/35.txt"
-url = raw_input("Please enter a valid url to mine from\n")
-response = urllib2.urlopen(url)
-webContent = response.read()
+#following function opens the url, reads it, and calls the strip module to remove html tags, punctuation.  
+#It also calls the count module to count the frequency of of the remaining words
+def mine(content):
+    response = urllib2.urlopen(content)
+    webContent = response.read()
 
-text = strip.StripTags(webContent).lower()
-final = strip.stripNonAlphaNum(text)
-#print text
-#print final
-#file = open("refined.txt","w")
-#file.write(str(final))
-#file.close()
-#for word in final:
- #   file = open("week-1-readings.txt","a")
-  #  file.write("\n" + word)
-   # file.close()
-counted = count.counting(final)
+    text = strip.StripTags(webContent).lower()
+    final = strip.stripNonAlphaNum(text)
+    counted = count.counting(final)
 
-print counted
+address = raw_input("Please enter a valid url to mine from\n")
+mine(address)
