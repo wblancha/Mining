34d647f8955776dd610cfce36f495c61844b3f91
wblancha <wblancha@gmail.com>
2015-01-28 23:27:56 -0500

    update

--- stopwords.py
+++ stopwords.py
@@ -2,11 +2,13 @@
 #tester = ["name", "date", "distance", "and", "or", "the"]
 def stop(words):
     filtered = []
-    stopwords =["and", "or", "a", "the", "then"]
+    source = open("stopwords.txt")
+    stopwords = source.read()
     for word in words:
         if word not in stopwords:
             filtered.append(word)
         else:
             print "Not in list"
     return filtered
+    source.close()
 #test = stop(tester)
